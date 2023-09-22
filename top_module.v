c7880b181e0d8405c5ad1dec1a5711712dab8bc4
Seungbeom <31874379+tmdqja1001@users.noreply.github.com>
2023-09-22 12:43:20 +0900

    Update top_module.v
    for vcs

--- top_module.v
+++ top_module.v
@@ -2,7 +2,7 @@ module top_module ();
     // A testbench
     reg [31:0] a=0;
     reg [31:0] b=0;
-    reg [31:0] out;
+    wire [31:0] out;
     reg [31:0] c1=0;
     reg clk;
     reg reset;
@@ -14,7 +14,13 @@ module top_module ();
         
     end
     
-    always #5 clk = ~clk;
+    always begin
+      #5 clk = ~clk;
+      $display("%d ns a signal is %h", $time, a);
+      $display("%d ns b signal is %h", $time, b);
+      $display("%d ns c1 signal is %h", $time, c1);
+      $display("%d ns out signal is %h", $time, out);
+    end
     
     initial begin
         //`probe_start;
@@ -70,16 +76,7 @@ module top_module ();
         c1 <= 32'b01000010111111011000110110100110;
         
         #50 $finish; // Quit the simulation
-        //`probe_stop;
-
     end
-    /*
-    `probe(a);
-    `probe(b);
-    `probe(c1);
-    `probe(clk);
-    `probe(reset);
-    */
     adder add(a, b, clk, reset, out); // Sub-modules work too.
 endmodule
 
@@ -90,21 +87,21 @@ module adder(
     input        reset,
     output [31:0] out);
 
-    reg a_sign;
-    reg [7:0] a_exponent;
-    reg [23:0] a_mantissa;
+    wire a_sign;
+    wire [7:0] a_exponent;
+    wire [23:0] a_mantissa;
     
-    reg b_sign;
-    reg [7:0] b_exponent;
-    reg [23:0] b_mantissa;
+    wire b_sign;
+    wire [7:0] b_exponent;
+    wire [23:0] b_mantissa;
 
-    reg o_sign;
-    reg [7:0] o_exponent;
-    reg [24:0] o_mantissa;
+    wire o_sign;
+    wire [7:0] o_exponent;
+    wire [24:0] o_mantissa;
     
-    reg  [7:0] o_exp;
-    reg  [24:0] o_man;
-    reg         o_s;
+    wire  [7:0] o_exp;
+    wire  [24:0] o_man;
+    wire         o_s;
 
     addition_align align1(
          .a(a),
@@ -143,24 +140,10 @@ module adder(
         .out_m_o(o_man),
         .out_s_o(o_s)
     );
-    
-    assign out[31] = o_s;
-    assign out[30:23] = o_exp;
-    assign out[22:0] = o_man[22:0];
 
-    /*
-    `probe(a);
-    `probe(b);
-    `probe(out);
-    `probe(a_mantissa);
-    `probe(b_mantissa);
-    `probe(a_exponent);
-    `probe(b_exponent);
-    `probe(o_mantissa);
-    `probe(o_exponent);
-    `probe(o_man);
-    `probe(o_exp);
-    */
+    assign  out[31] = o_s;
+    assign  out[30:23] = o_exp;
+    assign  out[22:0] = o_man[22:0];
 endmodule
 
 module addition_align(
@@ -168,75 +151,61 @@ module addition_align(
     input  [31:0] b,
     input         clk,
     input         reset,
-    output a_sign_o,
-    output b_sign_o,
-    output [7:0] a_exp_o,
-    output [7:0] b_exp_o,
-    output [23:0] a_man_o,
-    output [23:0] b_man_o);
+    output reg a_sign_o,
+    output reg b_sign_o,
+    output reg [7:0] a_exp_o,
+    output reg [7:0] b_exp_o,
+    output reg [23:0] a_man_o,
+    output reg [23:0] b_man_o);
 
-    reg a_sign;
-    reg b_sign;
-    reg [7:0] a_exp;
-    reg [7:0] b_exp;
-    reg [23:0] a_man;
-    reg [23:0] b_man;
-    
     always @ (posedge clk) begin        
         if (reset) begin
-            a_sign <= 0;
-            b_sign <= 0;
-            a_exp <= 0;
-            b_exp <= 0;
-            a_man <= 0;
-            b_man <= 0;
+            a_sign_o <= 0;
+            b_sign_o <= 0;
+            a_exp_o <= 0;
+            b_exp_o <= 0;
+            a_man_o <= 0;
+            b_man_o <= 0;
         end
         else begin
-        // IEEE 754's omitted bit in mantissa (1.E)
-        if(a[30:0] > b[30:0]) begin   // abs(A) > abs(B)
-              a_sign <= a[31];
-              if(a[30:23] == 0) begin // Underflow or zero
-                  a_exp <= 8'h01;
-                  a_man <= {1'b0, a[22:0]};
-              end else begin // Normal case
-                  a_exp <= a[30:23];
-                  a_man <= {1'b1, a[22:0]};
-              end
-              b_sign <= b[31];
-              if(b[30:23] == 0) begin
-                  b_exp <= 8'b00000001;
-                  b_man <= {1'b0, b[22:0]};
-              end else begin
-                  b_exp <= b[30:23];
-                  b_man <= {1'b1, b[22:0]};
-              end
-        end else begin
-              b_sign <= a[31];
-              if(a[30:23] == 0) begin // Underflow or zero
-                  b_exp <= 8'h01;
-                  b_man <= {1'b0, a[22:0]};
-              end else begin // Normal case
-                  b_exp <= a[30:23];
-                  b_man <= {1'b1, a[22:0]};
-              end
-              a_sign <= b[31];
-              if(b[30:23] == 0) begin
-                  a_exp <= 8'h01;
-                  a_man <= {1'b0, b[22:0]};
-              end else begin
-                  a_exp <= b[30:23];
-                  a_man <= {1'b1, b[22:0]};
-              end
-        end
+            // IEEE 754's omitted bit in mantissa (1.E)
+            if(a[30:0] > b[30:0]) begin   // abs(A) > abs(B)
+                a_sign_o <= a[31];
+                if(a[30:23] == 0) begin // Underflow or zero
+                    a_exp_o <= 8'h01;
+                    a_man_o <= {1'b0, a[22:0]};
+                end else begin // Normal case
+                    a_exp_o <= a[30:23];
+                    a_man_o <= {1'b1, a[22:0]};
+                end
+                b_sign_o <= b[31];
+                if(b[30:23] == 0) begin
+                    b_exp_o <= 8'b00000001;
+                    b_man_o <= {1'b0, b[22:0]};
+                end else begin
+                    b_exp_o <= b[30:23];
+                    b_man_o <= {1'b1, b[22:0]};
+                end
+            end else begin
+                b_sign_o <= a[31];
+                if(a[30:23] == 0) begin // Underflow or zero
+                    b_exp_o <= 8'h01;
+                    b_man_o <= {1'b0, a[22:0]};
+                end else begin // Normal case
+                    b_exp_o <= a[30:23];
+                    b_man_o <= {1'b1, a[22:0]};
+                end
+                a_sign_o <= b[31];
+                if(b[30:23] == 0) begin
+                    a_exp_o <= 8'h01;
+                    a_man_o <= {1'b0, b[22:0]};
+                end else begin
+                    a_exp_o <= b[30:23];
+                    a_man_o <= {1'b1, b[22:0]};
+                end
+            end
         end
     end
-
-    assign a_sign_o = a_sign;
-    assign b_sign_o = b_sign;
-    assign a_exp_o = a_exp;
-    assign b_exp_o = b_exp;
-    assign a_man_o = a_man;
-    assign b_man_o = b_man;
 
 endmodule
 
@@ -281,36 +250,30 @@ module addition_normaliser(
     input         in_s,
     input         clk,
     input         reset,
-    output      out_s_o,
-    output [7:0] out_e_o,
-    output [24:0] out_m_o);
+    output reg      out_s_o,
+    output reg [7:0] out_e_o,
+    output reg [24:0] out_m_o);
 
-    reg      out_s;
-    reg [7:0] out_e;
-    reg [24:0] out_m;
     wire [4:0] lzc;
 
     LZC_24 lzcnt1( .x(in_m[23:0]) , .cnt_o(lzc));
     
     always @ (posedge clk) begin
         if (reset) begin
-            out_s <= 0;
-            out_e <= 0;
-            out_m <= 0;
+            out_s_o <= 0;
+            out_e_o <= 0;
+            out_m_o <= 0;
         end else if(in_m[24]) begin
-            out_s <= in_s;
-            out_e <= in_e + 1;
-            out_m <= in_m >> 1;
+            out_s_o <= in_s;
+            out_e_o <= in_e + 1;
+            out_m_o <= in_m >> 1;
         end else begin
-            out_s <= in_s;
-            out_e <= in_e - lzc;
-            out_m <= in_m << lzc;
+            out_s_o <= in_s;
+            out_e_o <= in_e - lzc;
+            out_m_o <= in_m << lzc;
         end
      end
-	
-    assign out_s_o = out_s;
-    assign out_e_o = out_e;
-    assign out_m_o = out_m;
+
 endmodule
 
 module LZC_24(
@@ -327,14 +290,6 @@ module LZC_24(
             LZC_4 unit(.x(x[i+3:i]), .az_o(az_flag[i/4]), .q_o(q[i/2 + 1:i/2]));
         end
     endgenerate
-    /*
-    LZC_4 unit5(.x(x[23:20]), .az_o(az_flag[5]), .q_o(q[11:10]));
-    LZC_4 unit4(x[19:16], az_flag[4], q[9:8]);
-    LZC_4 unit3(x[15:12], az_flag[3], q[7:6]);
-    LZC_4 unit2(x[11:8], az_flag[2], q[5:4]);
-    LZC_4 unit1(x[7:4], az_flag[1], q[3:2]);
-    LZC_4 unit0(x[3:0], az_flag[0], q[1:0]);
-    */
 
     always @ (*) begin
         casex(az_flag)
@@ -361,5 +316,4 @@ module LZC_4(
     assign az_o = ~ ( x[3] | x[2] | x[1] | x[0] ); // high when all zero
     assign q_o[1]  = ~ ( x[3] | x[2] );
     assign q_o[0]  = ~x[3] & (~x[1] | x[2]);
-
 endmodule
